a47d0856d754827f0929b3ea213752248a70db8f
chentsungheng <chentsungheng@hotmail.com>
2015-04-25 17:30:07 +0800

    20150425 commit 06

---
+++ MvcApplication2/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using MvcApplication2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    // abstract controller不會被執行
+    public abstract class BaseController : Controller
+    {
+        protected ClientRepository repClient = RepositoryHelper.GetClientRepository();
+        protected OccupationRepository repOccupation = RepositoryHelper.GetOccupationRepository();
+
+#if DEBUG
+        public abstract JsonResult Debug();
+#endif
+    }
+}
