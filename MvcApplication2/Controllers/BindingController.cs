9b17ae118a4c442a3c751281f4d95e9e6c3873b9
chentsungheng <chentsungheng@hotmail.com>
2015-04-26 13:49:35 +0800

    20150426 commit 01

---
+++ MvcApplication2/Controllers/BindingController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class BindingController : Controller
+    {
+        // GET: Binding
+        public ActionResult Index()
+        {
+            ViewData["ViewMsg"] = "I am a View Message";
+            TempData["TempMsg"] = "I am a Temp Message";
+
+            return RedirectToAction("Show");
+        }
+
+        public ActionResult Show()
+        {
+            ViewBag.ViewMsg = ViewData["ViewMsg"];
+            ViewBag.TempMsg = TempData["TempMsg"];
+
+            return View();
+        }
+
+        public ActionResult Model()
+        {
+            MvcApplication2.Models.FabricsEntities db = new Models.FabricsEntities();
+
+            ViewData.Model = db.Products.Find(5);
+
+            return View();
+        }
+    }
+}
--- MvcApplication2/Controllers/TestController.cs
+++ MvcApplication2/Controllers/TestController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MvcApplication2.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,6 +30,72 @@ namespace MvcApplication2.Controllers
         public ContentResult Direct()
         {
             return Content("Test Content");
+        }
+
+        public FileResult File01()
+        {
+            return File(Server.MapPath("~/Content/MVC_1.png"), "image/png");
+        }
+
+        public FileResult File02()
+        {
+            byte[] file = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/MVC_1.png"));
+
+            return File(file, "image/png");
+        }
+
+        public FileResult File03()
+        {
+            FileStream fileStream = System.IO.File.OpenRead(Server.MapPath("~/Content/MVC_1.png"));
+
+            return File(fileStream, "image/png");
+        }
+
+        public FileResult Download()
+        {
+            FileStream fileStream = System.IO.File.OpenRead(Server.MapPath("~/Content/MVC_1.png"));
+
+            return File(fileStream, "image/png", "Stream.png");
+        }
+
+        public FileResult Plain()
+        {
+            return File(Server.MapPath("~/Content/HtmlPage1.html"), "text/plain");
+        }
+
+        private FabricsEntities db = new FabricsEntities();
+
+        public JsonResult Json01()
+        {
+            // Important!!!
+            db.Configuration.LazyLoadingEnabled = false;
+
+            return Json(db.Products.Take(20), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Redirect01()
+        {
+            return RedirectToAction("Plain");
+        }
+
+        public ActionResult Redirect02()
+        {
+            return RedirectToAction("Plain", new { url = "http://tw.yahoo.com" });
+        }
+
+        public ActionResult Redirect03()
+        {
+            return RedirectToActionPermanent("Plain", new { url = "http://tw.yahoo.com" });
+        }
+
+        public HttpNotFoundResult Test404()
+        {
+            return HttpNotFound("404 Test");
+        }
+
+        public ActionResult HttpCode()
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.Created, "Created test");
         }
     }
 }
