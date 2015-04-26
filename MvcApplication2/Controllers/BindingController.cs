d1f56537a53970484f03e51d097a0d4dff9a4075
chentsungheng <chentsungheng@hotmail.com>
2015-04-26 14:20:58 +0800

    20150426 commit 02

--- MvcApplication2/Controllers/BindingController.cs
+++ MvcApplication2/Controllers/BindingController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcApplication2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -32,6 +33,50 @@ namespace MvcApplication2.Controllers
             ViewData.Model = db.Products.Find(5);
 
             return View();
+        }
+
+        public ActionResult Base()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Base(string UserName, string Password)
+        {
+            return Content(string.Format("Base type ---> Username is {0}, Password is {1}", UserName, Password));
+        }
+
+        public ActionResult Form()
+        {
+            return View("Base");
+        }
+
+        [HttpPost]
+        public ActionResult Form(FormCollection PostForm)
+        {
+            return Content(string.Format("Form type ---> Username is {0}, Password is {1}", PostForm["UserName"], PostForm["Password"]));
+        }
+
+        public ActionResult typed()
+        {
+            return View("Base");
+        }
+
+        [HttpPost]
+        public ActionResult typed(DemoViewModel ViewModel)
+        {
+            return Content(string.Format("Typed ---> Username is {0}, Password is {1}", ViewModel.UserName, ViewModel.Password));
+        }
+
+        public ActionResult DoubleBinding()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoubleBinding(DemoViewModel Item1, DemoViewModel Item2)
+        {
+            return Content(string.Format("Double ---> Username1 is {0}, Password1 is {1}, Username2 is {2}, Password2 is {3}", Item1.UserName, Item1.Password, Item2.UserName, Item2.Password));
         }
     }
 }
---
+++ MvcApplication2/Models/DemoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    public class DemoViewModel : IValidatableObject
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string Confirm { get; set; }
+
+        [Required]
+        public int UserAge { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            yield break;
+        }
+    }
+}
